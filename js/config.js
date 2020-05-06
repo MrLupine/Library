9efcd469cc7cb74b4fcef6e8a7734499a7bde32d
Josse Pickard <jossepickard@gmail.com>
2020-05-06 13:04:56 +0100

    Full website created as an offshoot of library project with Miguelo29, tweaked styling and functionality.

---
+++ js/config.js
@@ -0,0 +1,1 @@
+const config = "AIzaSyDC9f43s9zFTXW1hOo4eA9_6Io3h-NKFA8"
---
+++ js/scripts.js
@@ -0,0 +1,219 @@
+class Book {
+	constructor(title, authors, pageCount, averageRating, read, image){
+		this.title = title;
+		this.authors = authors;
+		this.pageCount = pageCount;
+		this.myRating = averageRating;
+		this.read = read;
+		this.image = image;
+	}
+}
+
+const append = (parent, el) => parent.appendChild(el);
+const compare = type => (a,b) => (a[type] > b[type]) ? 1 : (b[type] > a[type]) ? -1 : 0;
+const createNode = element => document.createElement(element);
+const deleteChildren = x => {
+  const node = document.getElementById(`${x}`);
+  while (node.firstChild) {
+    node.removeChild(node.lastChild);
+  }
+}
+const hideOptions = () => myLibrary.length > 1 ? document.getElementById("dropdowns").style.display = "flex" : document.getElementById("dropdowns").style.display = "none" 
+const htmlStarCreator = bookID => {
+	let innerHTML = "";
+	for (let i = 5; i >= 0.5; i -= 0.5){
+		innerHTML += `<input type="radio" id="${bookID}rating-${i}" name="rating" value="${i}">
+									<label for="${bookID}rating-${i}"></label>`;
+	}
+	return innerHTML
+}
+const ifImageBlankThumbnail = dataPath => dataPath ? dataPath.thumbnail : "images/not-available.svg";
+const ifImageBlankFullSize = dataPath => dataPath ? returnLargestImage(dataPath) : ""
+const ifTitleBlank = dataPath => dataPath ? dataPath : "";
+const ifAuthorBlank = dataPath => dataPath ? dataPath.join(', ').replace(/, ([^,]*)$/, ' & $1') : "";
+const ifRatingBlank = dataPath => dataPath ? dataPath.map(item => (item.type === "ISBN_13") ? item.identifier : "" ) : "";
+const readCheck = book => book.read ? document.getElementById(`label-text${myLibrary.indexOf(book)}`).innerHTML = "Read" : document.getElementById(`label-text${myLibrary.indexOf(book)}`).innerHTML = "Unread";
+const render = book => {
+	const gridContainer = document.getElementById("libraryGrid");
+	const gridRow = document.createElement("div");
+  gridRow.classList.add("library-local-perspective");
+  gridRow.innerHTML = 
+   `<div class="library-book">
+		 	<img src="${book.image}" class="library-book__front"></img>
+		 	<div class="library-book__back">
+			 	<div class="library-book__back--title">${book.title}</div>
+			  <div class="library-book__back--authors">${book.authors}</div>
+			  <div class="library-book__back--page-count">${book.pageCount}<br>pages</div>
+			  <div class="library-book__back--read-button">
+			  	<input type="checkbox" class="hidden-checkbox" id="readCheckbox${myLibrary.indexOf(book)}">
+			  	<p id="label-text${myLibrary.indexOf(book)}"></p>
+			  	<label class="read-button-label" for="readCheckbox${myLibrary.indexOf(book)}"></label>
+			  </div>
+			  <div class="library-book__back--rating">
+			  	<fieldset class="star-rating-group">
+			  		${htmlStarCreator(myLibrary.indexOf(book))}
+			  	</fieldset>
+			  </div>
+			  <button class="library-book__back--remove-button" id="deleteButton${myLibrary.indexOf(book)}" data-type="${myLibrary.indexOf(book)}">
+			  	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.96 25"><title>close-button 2</title><polygon points="24.96 2.44 22.56 0.04 12.5 10.1 2.4 0 0 2.4 10.1 12.5 0 22.59 2.4 25 12.5 14.9 22.56 24.96 24.96 22.56 14.9 12.5 24.96 2.44" style="fill:#FFFFFF"/></svg>
+			  </button>
+		  </div>
+		</div>`;
+  gridContainer.appendChild(gridRow);
+  document.getElementById(`${myLibrary.indexOf(book)}rating-${book.myRating}`).checked = true;
+  document.getElementById(`readCheckbox${myLibrary.indexOf(book)}`).checked = book.read
+  readCheck(book)
+  document.getElementById(`readCheckbox${myLibrary.indexOf(book)}`).addEventListener("change", (e) => {
+  	book.read = document.getElementById(e.target.id).checked;
+		readCheck(book)
+  })
+  document.getElementById(`deleteButton${myLibrary.indexOf(book)}`).addEventListener("click", (e) => {
+  	myLibrary.splice(e.target.dataset.type, 1);
+  	renderLibrary()
+  })
+}
+const renderLibrary = () => {
+  document.getElementById("libraryGrid").innerHTML =
+   `<button class="library-grid__add-book" id="addBook">
+			<svg id="add-book-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 85 120"><title>add-book</title><polygon points="58.2 58.5 44 58.5 44 44.3 41 44.3 41 58.5 26.8 58.5 26.8 61.5 41 61.5 41 75.7 44 75.7 44 61.5 58.2 61.5 58.2 58.5"/><polygon points="55.5 2 83 2 83 29.6 85 29.6 85 0 55.5 0 55.5 2"/><polygon points="2 29.6 2 2 28.7 2 28.7 0 0 0 0 29.6 2 29.6"/><polygon points="28.7 118 2 118 2 87.9 0 87.9 0 120 28.7 120 28.7 118"/><polygon points="83 87.9 83 118 55.5 118 55.5 120 85 120 85 87.9 83 87.9"/></svg>
+		</button>`
+  hideOptions()
+  myLibrary.forEach(book => render(book));
+}
+const returnLargestImage = object => object.medium ? object.medium : object.thumbnail ? object.thumbnail : "";
+/*const returnLargestImage = object => object.extraLarge ? object.extraLarge : object.large ? object.large : object.medium ? object.medium : object.small ? object.small : "";*/
+const returnButtonFunction = input => {
+	const inputValues = {
+		modalReset: ["closeSearch", "modal-overlay", "Escape", "closeLoginSignUp"],
+		bookSearch: ["searchButton", "Enter"],
+	}
+	for (let [key, value] of Object.entries(inputValues)) {
+		if (value.indexOf(input) !== -1) {
+			return `${key}`;
+		}
+	}
+}
+const sortValue = x => x.replace(/\s+/g, "")
+const toggleDOM = (element, className) => document.getElementById(`${element}`).classList.toggle(`${className}`);
+
+const buttonFunctionsMap = {
+	bookSearch: () => {
+		const search = document.getElementById("searchInput").value;
+		const ul = document.getElementById("search-results");
+		if (modal.searchVisible && search !== "") {
+			if (!document.getElementById("searchBar").classList.contains("search-mode")){
+				toggleDOM("searchBar", "search-mode");
+			}
+			deleteChildren("search-results")
+
+			fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${config}`)
+				.then(response => response.json())
+				.then(data => {
+					data.items.map(book => {
+						let li = createNode("li"),
+						   img = createNode("img"),
+						     p = createNode("p");
+						   
+						img.src = `${ifImageBlankThumbnail(book.volumeInfo.imageLinks)}`;
+						p.innerHTML = `<div class="search-item__info--title">${ifTitleBlank(book.volumeInfo.title)}</div><div class="search-item__info--authors">${ifAuthorBlank(book.volumeInfo.authors)}</div>`;
+						li.classList = "search-item";
+						img.classList = "search-item__image";
+						p.classList = "search-item__info";
+						li.addEventListener("click", (e) => {
+							fetch(`https://www.googleapis.com/books/v1/volumes/${book.id}?key=${config}`)
+								.then(response => response.json())
+								.then(data => {
+									myLibrary.push(
+										new Book(
+											ifTitleBlank(data.volumeInfo.title), 
+											ifAuthorBlank(data.volumeInfo.authors), 
+											data.volumeInfo.pageCount, 
+											data.volumeInfo.averageRating, 
+											false, 
+											ifImageBlankFullSize(data.volumeInfo.imageLinks)))
+									render(myLibrary[myLibrary.length - 1]);
+									hideOptions()
+								})
+						})
+						append(li, img);
+						append(li, p);;
+						append(ul, li);
+						})
+					})
+		}
+	},
+	modalReset: () => {
+		["searchBar", "modal-overlay", "loginSignUp"].forEach(element => {
+			if (!document.getElementById(element).classList.contains("hidden")) {
+				toggleDOM(element, "hidden")
+			}
+		})
+		if (document.getElementById("searchBar").classList.contains("search-mode")) {
+    	toggleDOM("searchBar", "search-mode");
+	  }
+	  document.getElementById("searchInput").value = "";
+	  modal.searchVisible = modal.loginSignUpVisible = false
+	},
+}
+
+const myLibrary = []
+
+const modal = {
+	searchVisible: false,
+  loginSignUpVisible: false,
+}
+
+window.addEventListener("load", () => {
+	  hideOptions()
+})
+
+document.addEventListener("click", (e) => {
+  if (returnButtonFunction(e.target.id)) {
+    buttonFunctionsMap[returnButtonFunction(e.target.id)]();
+  }
+})
+
+document.getElementById("addBook").addEventListener("click", () => {	
+  deleteChildren("search-results");
+  modal.searchVisible = true;
+  toggleDOM("searchBar", "hidden");
+  toggleDOM("modal-overlay", "hidden");
+})
+
+document.getElementById("loginSignUpButton").addEventListener("click", () => {
+    modal.loginSignUpVisible = true;
+    toggleDOM("loginSignUp", "hidden");
+    toggleDOM("modal-overlay", "hidden");
+})
+
+document.addEventListener("keyup", (e) => {
+  if (modal.searchVisible && returnButtonFunction(e.key)) {
+    buttonFunctionsMap[returnButtonFunction(e.key)]();
+  }
+})
+
+document.getElementById("hideRead").addEventListener("change", (e) => {
+	const innerHTML =
+		`<button class="library-grid__add-book" id="addBook">
+			<svg id="add-book-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 85 120"><title>add-book</title><polygon points="58.2 58.5 44 58.5 44 44.3 41 44.3 41 58.5 26.8 58.5 26.8 61.5 41 61.5 41 75.7 44 75.7 44 61.5 58.2 61.5 58.2 58.5"/><polygon points="55.5 2 83 2 83 29.6 85 29.6 85 0 55.5 0 55.5 2"/><polygon points="2 29.6 2 2 28.7 2 28.7 0 0 0 0 29.6 2 29.6"/><polygon points="28.7 118 2 118 2 87.9 0 87.9 0 120 28.7 120 28.7 118"/><polygon points="83 87.9 83 118 55.5 118 55.5 120 85 120 85 87.9 83 87.9"/></svg>
+		</button>`
+	if (e.target.checked) {
+		document.getElementById("libraryGrid").innerHTML = innerHTML;
+  	myLibrary.forEach(book => {if (!book.read) render(book)})
+	} else {
+		document.getElementById("libraryGrid").innerHTML = innerHTML;
+  	myLibrary.forEach(book => render(book))
+	}
+})
+
+document.querySelectorAll(".switchForm").forEach(form => {
+	form.addEventListener("click", () => {
+		toggleDOM("signUp", "hidden");
+		toggleDOM("logIn", "hidden");
+	})
+})
+
+document.getElementById("sortList").addEventListener("change", (e) => {
+  myLibrary.sort(compare(sortValue(e.target.value)));
+  renderLibrary();
+})
